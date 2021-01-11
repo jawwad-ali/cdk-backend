c5c6dee59d4002ddb67bd2dc4babb039399f0e66
jawwad-ali <alijawwad001@gmail.com>
2021-01-11 21:18:56 +0500

    first commit

--- lib/cdkbackend-stack.ts
+++ lib/cdkbackend-stack.ts
@@ -1,9 +1,17 @@
 import * as cdk from '@aws-cdk/core';
+import * as ddb from "@aws-cdk/aws-dynamodb"
 
 export class CdkbackendStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+    const dynamoDBTable = new ddb.Table(this, 'Table', {
+      tableName: "table-codepipeline",
+      partitionKey: {
+        name: 'id',
+        type: ddb.AttributeType.STRING,
+      },
+    });
   }
 }
